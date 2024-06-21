25fe71eabefaf34ee15d1e985799550e84521712
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2024-06-21 09:50:35 +0530

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import "./style.css";
+import * as React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Header() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container id="header">
+        <Navbar.Brand href="#home" className="sm-3">HackerLab Pro</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
